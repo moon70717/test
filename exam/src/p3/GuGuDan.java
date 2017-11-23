a1a3b57944a4430e007e5be2039498b5f5a14b74
Administrator <Administrator@DJA-PC>
2017-11-23 18:29:59 +0900

    3일차

--- exam/src/p3/GuGuDan.java
+++ exam/src/p3/GuGuDan.java
@@ -1,21 +1,17 @@
 package p3;
 
 public class GuGuDan {
-	public static void main(String[]args) {
-		for(int i=1;i<10;i++) {
-//			System.out.print(i+"´Ü||");
-			for(int j=1;j<=9;j++) {
-//				System.out.print(i+" * "+j+" = "+(j*i)+",  ");
-				System.out.printf("%2d * %2d = %2d||", j,i,(i*j));
+	void printLoop(ObjectExam oe) {
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				// System.out.printf("%2d * %2d = %2d ||", j,i,(i*j));
+				System.out.print("[" + i + ", " + j + "]");
 			}
 			System.out.println("");
-//			System.out.println(i+" * 9 = "+(9*i));
 		}
+	}
+
+	public static void main(String[] args) {
 		
-		/*for(int i=1;i<10;i++) {
-			for(int j=1;j<10;j++) {
-				System.out.print(j+"*"+i+"="+(i*j)+", ");
-			}System.out.println("");
-		}*/
 	}
 }
--- exam/src/p3/ObjectExam.java
+++ exam/src/p3/ObjectExam.java
@@ -1,7 +1,41 @@
 package p3;
 
+import java.util.Scanner;
+
 public class ObjectExam {
+	int num1;
+	int num2;
+
+	ObjectExam() {
+		System.out.println("생성자 호출");
+	}
+
+	void add(int num1, int num2) {
+		System.out.println("둘의 합은 " + (num1 + num2));
+	}
+
+	int getNum2(int num2) {
+		return num2;
+	}
+
+	void inputNums() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("첫번째 숫자를 입력해 주세요 : ");
+		num1 = sc.nextInt();
+		System.out.println("두번째 숫자를 입력해 주세요 : ");
+		num2 = sc.nextInt();
+	}
+
+	void printLoop() {
+		for (int i = num1; i < num2; i++) {
+			System.out.print(i + ",");
+		}
+		System.out.println(num2);
+	}
+
 	public static void main(String[] args) {
-		ObjectExam oe = new ObjectExam();
+		ObjectExam objectExam = new ObjectExam();
+		objectExam.inputNums();
+		objectExam.printLoop();
 	}
 }

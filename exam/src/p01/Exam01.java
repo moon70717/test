f44c62d3297f4323f50e69117829b883610a2936
Administrator <Administrator@DJA-PC>
2017-11-23 10:45:34 +0900

    a

---
+++ exam/src/p01/Exam01.java
@@ -0,0 +1,13 @@
+package p01;
+
+public class Exam01 {
+	public static void main(String[] args) {
+		int a=3;
+		int b=4;
+		System.out.println(a*b);
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}

fab7c86b9d7cd8109ea88e01b6967b02809227ea
Administrator <Administrator@DJA-PC>
2017-11-27 15:30:09 +0900

    5

--- exam/src/ch07/ExtendsExam.java
+++ exam/src/ch07/ExtendsExam.java
@@ -1,7 +1,18 @@
 package ch07;
 
-public class ExtendsExam {
-	public static void main(String[]args) {
+class father {
+	int i=3;
+}
+
+public class ExtendsExam extends father{// father클레스를 상속받음
+	
+	void use() {
+		System.out.println(i);//상속받은 클래스의 변수를 사용가능함
 		
+		System.out.println(--i);//상속받은 클래스의 변수를 수정할수 있음
+	}
+	
+	public static void main(String[]args) {
+		new ExtendsExam().use();
 	}
 }

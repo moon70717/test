16afb514c769962613194b122734116ff4b94b8d
Administrator <Administrator@DJA-PC>
2017-11-27 11:39:02 +0900

    5

---
+++ exam/src/ch06/InstanceMethod.java
@@ -0,0 +1,34 @@
+package ch06;
+
+public class InstanceMethod {
+	static int sInt=0;
+	int insInt=0;
+	
+	int insInt2=insInt;
+	//static int sInt2=insInt2; 생성자가 필요함
+	static int sInt2=new InstanceMethod().insInt2;//이렇게
+	
+	static void sInt() {
+		System.out.println(sInt);
+		//insInt(); 생성자가 필요하므로 불가능함
+	}
+	
+	void insInt() {
+		System.out.println(insInt);//인스턴스 메서드이므로 인스턴스 변수를 그냥 사용가능
+	}
+	
+	void calling() {
+		sInt();
+		insInt();
+	}
+	
+	public static void main(String[]args) {
+		InstanceMethod ins=new InstanceMethod();
+		sInt=3;
+		ins.insInt=2;
+		sInt();//static은 생성자 필요없음, static 메서드라고함
+		ins.insInt();//static이 없을경우 생성자 필요, 인스턴스메서드라고 함
+		
+		ins.calling();//인스턴스 메서드에서는 두종류의 메서드를 둘다 호출가능
+	}
+}
--- exam/src/p05/ArrayExam.java
+++ exam/src/p05/ArrayExam.java
@@ -1,7 +1,27 @@
 package p05;
 
 public class ArrayExam {
-	public static void main(String[]args) {
-		
+	public static void main(String[] args) {
+		int[][] numArr = new int[3][3];
+		numArr[0][0] = 1;
+		numArr[0][1] = 2;
+		numArr[0][2] = 3;
+		numArr[1][0] = 4;
+		numArr[1][1] = 5;
+		numArr[1][2] = 6;
+		numArr[2][0] = 7;
+		numArr[2][1] = 8;
+		numArr[2][2] = 9;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				numArr[i][j] = j + 1;
+				System.out.print(numArr[i][j]);
+				if(j!=2)
+					System.out.print(", ");
+				else
+					System.out.println();
+			}
+		}
 	}
 }
